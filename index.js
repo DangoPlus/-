4c13d6498b3a16fe81d7731fb96da44f22e1cf29
DangoPlus <depengyu@gmail.com>
2022-06-24 07:32:24 +0000

    add a good example

--- index.js
+++ index.js
@@ -34,4 +34,42 @@ const arrayToTree = (data, pid) => {
   return result;
 }
 
-console.log(JSON.stringify(arrayToTree(arr,0)))
+
+function bestArrayToTree(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = {
+        children: [],
+      }
+    }
+
+    itemMap[id] = {
+      ...item,
+      children: itemMap[id]['children']
+    }
+
+    const treeItem =  itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
+
+
+// console.log(JSON.stringify(arrayToTree(arr,0)));
+console.log(JSON.stringify(bestArrayToTree(arr)));
